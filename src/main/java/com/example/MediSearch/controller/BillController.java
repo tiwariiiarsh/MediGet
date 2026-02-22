10b82a1079c6b5fcb029ec7b618bc21af5c47803
Arsh <tiwariiiarsh1106@gmail.com>
2026-02-22 18:44:39 +0530

    BillController is added

---
+++ src/main/java/com/example/MediSearch/controller/BillController.java
@@ -0,0 +1,44 @@
+package com.example.MediSearch.controller;
+
+import com.example.MediSearch.payload.CartItemDTO;
+import com.example.MediSearch.service.BillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/seller/shop")
+public class BillController {
+
+    @Autowired
+    private BillService billService;
+
+    @PostMapping("/{shopId}/bill")
+    public ResponseEntity<?> generateBill(
+            @PathVariable Long shopId,
+            @RequestBody List<CartItemDTO> cartItems) {
+
+        return ResponseEntity.ok(
+                billService.generateBill(
+                        shopId,
+                        cartItems));
+    }
+
+    @GetMapping("/{shopId}/analytics/best")
+    public ResponseEntity<String> bestSelling(
+            @PathVariable Long shopId) {
+
+        return ResponseEntity.ok(
+                billService.getBestSelling(shopId));
+    }
+
+    @GetMapping("/{shopId}/analytics/least")
+    public ResponseEntity<String> leastSelling(
+            @PathVariable Long shopId) {
+
+        return ResponseEntity.ok(
+                billService.getLeastSelling(shopId));
+    }
+}
