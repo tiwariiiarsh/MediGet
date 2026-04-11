dfd7c5fe9eecde1bb433c3bf98066ab384f0ada3
Arsh <tiwariiiarsh1106@gmail.com>
2026-04-11 16:50:53 +0530

    AnalyticsDTO added

--- src/main/java/com/example/MediSearch/payload/AnalyticsDTO.java
+++ src/main/java/com/example/MediSearch/payload/AnalyticsDTO.java
@@ -1,4 +1,14 @@
 package com.example.MediSearch.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data @AllArgsConstructor @NoArgsConstructor
 public class AnalyticsDTO {
+    private String medicineName;
+    private Long totalSold;
+    private Double revenue;
+    private Integer currentStock;
 }
